ca295396fcfd6b5b03f26b6078386ab51c5d5867
Tim Panton <tpanton@voxeolabs.com>
2013-12-03 15:20:31 +0000

    Messed with it to add google play services

--- client/src/uk/co/westhawk/pulley/PulleySetup.java
+++ client/src/uk/co/westhawk/pulley/PulleySetup.java
@@ -9,11 +9,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class PulleySetup extends Activity {
 
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     /**
      * Called when the activity is first created.
      */
@@ -25,6 +29,25 @@ public class PulleySetup extends Activity {
      }
      */
     Button btn;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        int ret = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        switch (ret) {
+            case ConnectionResult.SUCCESS:
+                Log.d("Pulley", "Google play services available");
+                break;
+            case ConnectionResult.SERVICE_MISSING:
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+            case ConnectionResult.SERVICE_DISABLED:
+            case ConnectionResult.SERVICE_INVALID:
+            case ConnectionResult.DATE_INVALID:
+                Log.d("Pulley", "Problem with Google play services");
+                GooglePlayServicesUtil.getErrorDialog(ret, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+                break;
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
